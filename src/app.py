a265e299c17aa40ae3790d4e2e1b87783b67ee5f
Kelsey Low <helloklow@gmail.com>
2024-12-10 01:07:49 +0000

    move files

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from flask import Flask, request, render_template
 
 # Load the model
-model_file='/models/model.pkl'
+model_file='../models/model.pkl'
 
 with open(model_file, 'rb') as input_file:
     model=pickle.load(input_file)
